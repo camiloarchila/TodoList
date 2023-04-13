14b3ee73f10cca6652b40aecdb0c46020e47cd4d
camilo <camiloa@innfuturum.com>
2023-04-12 21:46:34 -0500

    Custom Hooks

--- src/Components/TaskList.jsx
+++ src/Components/TaskList.jsx
@@ -1,42 +1,22 @@
-import { useEffect, useState } from "react";
+
 import { TaskCard } from "./TaskCard"
+import { useTaskList } from "../hooks/useTaskList";
 
 export const TaskList = (prop) => {
 
     const {list, onDeletetask} = prop;
-    const [tasks, setTasks] = useState([]);
+    const [tasks, checkTast, deleteTask] = useTaskList([]);
 
     const handleCheckClick = (name) =>{
-
-        let taskschecked = [...tasks];
-
-        if(!tasks.includes(name)){
-            taskschecked = [...tasks, name];
-        }else{
-            taskschecked = taskschecked.filter(task => name != task);
-        }
-
-        setTasks(taskschecked)
-
-        localStorage.setItem("Checked", JSON.stringify(taskschecked))
+        checkTast(name);
     }
 
     const handleDeleteCick = (name) =>{
-        let taskschecked = [...tasks];
-        taskschecked = taskschecked.filter(task => name != task);
-        setTasks(taskschecked)
-        localStorage.setItem("Checked", JSON.stringify(taskschecked))
+        deleteTask(name);
         onDeletetask(name);
     }
 
-    useEffect(() => {
-        const datastored = localStorage.getItem("Checked");
-        const datatasksChecked = JSON.parse(datastored);
-        if(datatasksChecked != null){
-        setTasks(datatasksChecked);
-        }
-    }, [])
-
+    
     return <ul>
         {
             list.map((task) => <TaskCard name={task.name} onCheckClick={handleCheckClick}  onClickDelete={handleDeleteCick} isClicked={tasks.includes(task.name)}/>)
