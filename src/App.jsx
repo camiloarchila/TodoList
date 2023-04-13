be6477cd84384a07a18bba10a117e996da9495fd
camilo <camiloa@innfuturum.com>
2023-04-12 22:11:57 -0500

    Lista de tareas

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,30 @@ import { useCreateTask } from "./hooks/useTaskList"
 function App() {
 
   const [newtasks, addTask, deleteTask, deleteAll] = useCreateTask([]);
+  const [description, setDesciption] = useState('');
   const [value, setvalue] = useState('');
+  const [formValidation, setFormValidation] = useState({
+    task:undefined,
+    description:undefined,
+  })
 
-  const handlechange = (event) => {
-    setvalue(event.target.value);
+  function handlechange (event) {
+    const nametask = event.target.value;
+    setFormValidation({...formValidation, task: nametask.length < 3 ? "Name Task is too short" : ""})
+    setvalue(nametask);
   }
 
-  const handleClickButton = (event) => {
+  function handleDescriptionChange(event){
+    const valueDescription = event.target.value;
+    setFormValidation({...formValidation, description:valueDescription.length === 0? "Description is required" : "",})
+    setDesciption(valueDescription);
+  }
+
+  function handleSubmit(event){
     event.preventDefault();
-    addTask(value);
-    setvalue('');
+    addTask(value)
+    setvalue("")
+    setDesciption("")
   }
 
   const handleDeleteItem = (itemdeleted) => {
@@ -27,11 +41,26 @@ function App() {
     deleteAll();
   }
 
+  const isValid = Object.keys(formValidation).every(key=>formValidation[key]==="")
+
   return (
     <div >
       <Header/>
-      <input type="text" id="inputtask" value={value} placeholder="Add your new todo" onChange={handlechange}></input> 
-      <button onClick={handleClickButton}>+</button>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Task
+            <input type="text" id="inputtask" value={value} placeholder="Add your new todo" onChange={handlechange}></input> 
+          </label>
+          <span role = "alert" style={{color:"red"}}>{formValidation.task}</span>
+        </div>
+        <div>
+          <label> Description
+            <input type= "text" value = {description} onChange={handleDescriptionChange} ></input>
+          </label>
+          <span role = "alert" style={{color:"red"}}>{formValidation.description}</span>
+        </div>
+        <button disabled={!isValid} type="submit">Agregar Tarea</button>
+      </form>
       <TaskList list={newtasks} onDeletetask={handleDeleteItem}/> 
       <button onClick={handleDeleteAll}>Delete All</button> 
     </div>
